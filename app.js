f2db29c4b8e90bed445d32cfb48eae11d27262e3
julieta-bottale <julietabottale@hotmail.com>
2023-02-05 11:50:02 -0300

    cree repo remoto

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+app.listen(3030,()=> 
+console.log ("Servidor corriendo en el puerto 3030")); 
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res){
+    res.sendFile (path.join(__dirname,"/views/home.html"));
+})
+
